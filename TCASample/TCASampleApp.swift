f029d5fe9789c1904f17b6dd9fa2248455328af2
YoHan Cho <devyhan93@gmail.com>
2022-11-21 15:25:12 +0900

    Initial Commit

---
+++ TCASample/TCASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASampleApp.swift
+//  TCASample
+//
+//  Created by 조요한 on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TCASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
